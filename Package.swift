bd96a2bfd9fe9b20a178a2be2909faa6d5383998
Mukesh Soni <mukeshsoni@gmail.com>
2023-08-01 16:49:50 +0530

    Let's get the ball rolling

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "screencapturekit-cli",
+    platforms: [.macOS(.v13)],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", exact: "1.2.2"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "screencapturekit-cli",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ],
+            path: "Sources"
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,13 @@
+This project provides a command line interface to Apple's [`ScreenCaptureKit`](https://developer.apple.com/documentation/screencapturekit/).
+
+## Build
+
+```bash
+swift build
+```
+
+## Run
+```bash
+swift run
+```
+
